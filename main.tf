f4382422c5d15687f8a087bc16c6f72ae33bab64
Rahul <srahul3@gmail.com>
2024-07-01 17:05:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -298,3 +298,23 @@ resource "aws_route53_record" "nomad_client" {
   ttl     = 300
   records = [aws_instance.client[0].public_ip]
 }
+
+data "hcp_packer_version" "ubuntu" {
+  bucket_name  = "learn-packer-run-tasks"
+  channel_name = "production"
+}
+
+data "hcp_packer_artifact" "ubuntu_us_east_2" {
+  bucket_name         = "learn-packer-run-tasks"
+  platform            = "aws"
+  version_fingerprint = data.hcp_packer_version.ubuntu.fingerprint
+  region              = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.hcp_packer_artifact.ubuntu_us_east_2.external_identifier
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Learn-HCP-Packer"
+  }
+}
